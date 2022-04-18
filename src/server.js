2a8702134a8125b37220e0bef6b9100e3926decf
Ulugbek <ulug13ek@Ulugbeks-MacBook-Pro.local>
2022-04-18 17:08:43 +0500

    server created

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import authRouter from "./routes/auth.js"
+
+const app = express()
+app.use(express.json())
+app.use(authRouter)
+
+
+
+app.listen(4000, ()=> console.log("Server is running http://localhost:4000"))
